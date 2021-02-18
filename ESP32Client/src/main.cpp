8eefbde1515d60b0f233d5d1a27ee40a675a2780
math228 <78847152+math228@users.noreply.github.com>
2021-02-18 14:20:40 +0100

    Add files via upload

---
+++ ESP32Client/src/main.cpp
@@ -0,0 +1,63 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-client-server-wi-fi/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "ESP32Client.h"
+#include "Wire.h"
+#include "SPI.h"
+#include "FS.h"
+const char* ssid = "ESP32-Access-Point";
+const char* password = "123456789";
+
+//Your IP address or domain name with URL path
+const char* serverNameTemp = "http://192.168.4.1/temperature";
+const char* serverNameAccelx = "http://192.168.4.1/AccelX";
+
+String temperature;
+String AccelX;
+
+unsigned long previousMillis = 0;
+const long interval = 5000; 
+
+void setup() {
+  Serial.begin(9600);
+  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  
+  if(currentMillis - previousMillis >= interval) {
+     // Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED ){ 
+     temperature = httpGETRequest(serverNameTemp);
+     AccelX = httpGETRequest(serverNameAccelx);
+      Serial.println("Temperature: " + temperature + " Accelx: "+ AccelX);
+      
+      // save the last HTTP GET Request
+      previousMillis = currentMillis;
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+  }
+}
+
